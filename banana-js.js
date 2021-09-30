22aede9e9c89982d6c3d9b6014211e046a4fffd9
himanshu0089 <88453380+himanshu0089@users.noreply.github.com>
2021-09-30 12:11:59 +0530

    Initial commit

---
+++ banana-js.js
@@ -0,0 +1,33 @@
+var translateBtn = document.querySelector("#translate-btn");
+var text = document.querySelector("#text");
+var translate = document.querySelector("#trans-text");
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler() {
+    var inputText = text.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            translate.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
+
+translateBtn.addEventListener("click", clickHandler)
